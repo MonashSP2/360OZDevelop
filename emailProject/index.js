8f601c895114a59557f2ed907ce40cd80cc4f1b7
pson0001 <pson0001@hotmail.com>
2018-09-30 00:38:02 +1000

    email example

---
+++ emailProject/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/api/form', (req, res) =>{
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Name: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.live.com',
+      port: 587,
+      secure: false, 
+      auth: {
+        user: 'ping.s19@hotmail.com', // generated ethereal user
+        pass: 'songsongsong00' // generated ethereal password
+      },
+
+    });
+    let mailOptions = {
+      from: 'ping.s19@hotmail.com', // sender address
+      to: req.body.email, // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: htmlEmail // html body
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', {msg:'Email has been sent'});
+    });
+
+  })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, ()=>{
+  console.log(`listen to 3000`);
+})
